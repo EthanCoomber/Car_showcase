3fdf8245b762c04003371726151f9aeeec27879a
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-01-26 20:38:27 -0500

    updated logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
 import "./globals.css";
+import Head from "next/head";
 
 import { Footer, NavBar } from "@components";
 
 export const metadata = {
   title: "Car Hub",
   description: "Discover world's best car showcase application",
+  // image: "./favicon.ico"
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <Head>
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
       <body className='relative'>
         <NavBar />
         {children}
---
+++ components/HeadMetaTags.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function HeadMetaTags() {
+  return (
+    <Head>
+      <link rel="shortcut icon" href="../app/favicon.ico" />
+    </Head>
+  )
+}
